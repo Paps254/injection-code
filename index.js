bdb66efe95f117c5ae4f6aaa9236d9c377daf20d
KUNI <kunijeremy@gmail.com>
2023-04-04 12:10:47 +0300

    Models

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const Sequelize =require('sequelize');
 
 const sequelize = new Sequelize('sequelize_video','root', '',{
     dialect: 'mysql',
+    define:{
+        freezeTableName:true
+    }
 })
 
-sequelize.authenticate().then(()=>{
-    console.log('connection successful');
-    }).catch(err=>{
-        console.log('connection error',err);
-})
+// sequelize.sync({force:true});
+
+// sequelize.authenticate().then(()=>{
+//     console.log('connection successful');
+//     }).catch(err=>{
+//         console.log('connection error',err);
+// })
 
 // async function myFunction(){
 //     await sequelize.authenticate();
@@ -17,4 +22,41 @@ sequelize.authenticate().then(()=>{
 
 // myFunction();
 
-console.log("Another task");
+// console.log("Another task");
+
+
+const User = sequelize.define('user',{
+    user_id:{
+        type:Sequelize.DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+    },
+    username:{
+        type:Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    password:{
+        type:Sequelize.DataTypes.STRING,
+    },
+    age:{
+        type:Sequelize.DataTypes.INTEGER,
+        defaultValue:18 
+    },
+
+    // true or false,
+    // wittCodeRocks:{
+    //     type:Sequelize.DataTypes.BOOLEAN,
+    //     defaultValue:true
+    // }
+    
+},
+{
+    freezeTableName:true,
+    timestamps:false
+});
+
+User.sync({force:true}).then((data) => {
+    console.log("Table and model synced successfully!");
+}).catch((err) => {
+    console.log("Error syncing the table and model!");
+});
